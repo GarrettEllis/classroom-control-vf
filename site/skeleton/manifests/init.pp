ddffd757f1922b6ffee56969c0af183616bdfe57
Garrett Ellis <GarrettEllis@users.noreply.github.com>
2016-07-20 11:27:44 -0700

    Create init.pp

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,14 @@
+class skeleton {
+  file { '/etc/skel':
+    ensure => directory,
+    mode => '0755',
+    owner => 'root',
+    group => 'root',
+  }
+  file { '/etc/skel/.bashrc':
+    ensure => file,
+    mode => '0644',
+    owner => 'root',
+    group => 'root',
+    source => 'puppet:///skeleton/example.bashrc',
+  }
